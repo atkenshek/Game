47d48b7af876c658b0d8ab999feeb5da990a98fe
Meiram Sopy <71462628+atkenshek@users.noreply.github.com>
2021-09-17 15:31:26 +0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/L1_IA2001.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/Clionprojectsnew/Newest/Character.cpp" "F:/Clionprojectsnew/Newest/cmake-build-debug/CMakeFiles/L1_IA2001.dir/Character.cpp.obj"
+  "F:/Clionprojectsnew/Newest/Enemy.cpp" "F:/Clionprojectsnew/Newest/cmake-build-debug/CMakeFiles/L1_IA2001.dir/Enemy.cpp.obj"
+  "F:/Clionprojectsnew/Newest/Fight.cpp" "F:/Clionprojectsnew/Newest/cmake-build-debug/CMakeFiles/L1_IA2001.dir/Fight.cpp.obj"
+  "F:/Clionprojectsnew/Newest/Game.cpp" "F:/Clionprojectsnew/Newest/cmake-build-debug/CMakeFiles/L1_IA2001.dir/Game.cpp.obj"
+  "F:/Clionprojectsnew/Newest/Item.cpp" "F:/Clionprojectsnew/Newest/cmake-build-debug/CMakeFiles/L1_IA2001.dir/Item.cpp.obj"
+  "F:/Clionprojectsnew/Newest/main.cpp" "F:/Clionprojectsnew/Newest/cmake-build-debug/CMakeFiles/L1_IA2001.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/L1_IA2001.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L1_IA2001.dir/Character.cpp.obj"
+  "CMakeFiles/L1_IA2001.dir/Enemy.cpp.obj"
+  "CMakeFiles/L1_IA2001.dir/Fight.cpp.obj"
+  "CMakeFiles/L1_IA2001.dir/Game.cpp.obj"
+  "CMakeFiles/L1_IA2001.dir/Item.cpp.obj"
+  "CMakeFiles/L1_IA2001.dir/main.cpp.obj"
+  "L1_IA2001.exe"
+  "L1_IA2001.exe.manifest"
+  "L1_IA2001.pdb"
+  "libL1_IA2001.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L1_IA2001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
